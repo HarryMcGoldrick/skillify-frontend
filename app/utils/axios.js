bd36b901fc2fa2a47a98ad4bf888bbe26dc6b444
BuildTools <harry_mcgoldrick@msn.com>
2020-12-03 16:48:19 +0000

    update bearer signature to capital B

--- app/utils/axios.js
+++ app/utils/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 axios.defaults.baseURL = 'http://localhost:3000/';
-axios.defaults.headers.common = { Authorization: `bearer ${Cookies.get('access_token')}` };
+axios.defaults.headers.common = { Authorization: `Bearer ${Cookies.get('access_token')}` };
 export default axios;
