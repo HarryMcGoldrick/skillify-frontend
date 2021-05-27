fc198290dab13dabbedeb43672715047650ab07f
BuildTools <harry_mcgoldrick@msn.com>
2021-05-27 09:51:06 +0100

    fix edit nav

--- app/containers/graph-page-container/graph-page.jsx
+++ app/containers/graph-page-container/graph-page.jsx
@@ -1,8 +1,10 @@
 import { Drawer, Grid } from '@material-ui/core';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { Prompt } from 'react-router';
+import { Prompt, useParams } from 'react-router';
 import { GraphDetails, NodeDrawerPanel } from '../../components';
+import { getUserInfoByUsername } from '../../services/user-service';
+import { getExistingSession, getUserId, isAuthenticated } from '../../utils/authentication';
 import GraphContainer from '../graph-container/graph-container';
 import './graph-page.css';
 
@@ -13,45 +15,61 @@ const GraphPage = (props) => {
   const {
     selectedNode, showGraphDetails, viewOnly,
   } = props;
+  const [isVisible, setIsVisible] = useState(false);
+  const { id: graphId } = useParams();
 
   useEffect(() => {
     if (!viewOnly) {
-      // Triggers the prompt before unloading the page
-      window.onbeforeunload = () => true;
+      const { username } = isAuthenticated() ? getExistingSession() : '';
+      if (username) {
+        getUserInfoByUsername(username).then((data) => {
+          if (data) {
+            if (data.graphs_created.includes(graphId)) setIsVisible(true);
+          }
+          // Triggers the prompt before unloading the page
+          window.onbeforeunload = () => true;
+        });
+      }
+    } else {
+      setIsVisible(true);
     }
   }, []);
 
   return (
     <div>
-      <Grid container justify="center">
-        <Grid item>
-          <Drawer anchor="left" open={showGraphDetails} variant="persistent">
-            <GraphDetails viewOnly={viewOnly} />
-          </Drawer>
-        </Grid>
+      {isVisible && (
+        <>
+          <Grid container justify="center">
+            <Grid item>
+              <Drawer anchor="left" open={showGraphDetails} variant="persistent">
+                <GraphDetails viewOnly={viewOnly} />
+              </Drawer>
+            </Grid>
 
-        <Grid item>
-          <Drawer
-            anchor="right"
-            open={Boolean(selectedNode.id)}
-            variant="persistent"
-          >
-            <NodeDrawerPanel viewOnly={viewOnly} />
-          </Drawer>
-        </Grid>
+            <Grid item>
+              <Drawer
+                anchor="right"
+                open={Boolean(selectedNode.id)}
+                variant="persistent"
+              >
+                <NodeDrawerPanel viewOnly={viewOnly} />
+              </Drawer>
+            </Grid>
 
-        {/* Inline style has to be used here unfortunately */}
-        <Grid item className={showGraphDetails ? 'drawer-open' : 'drawer-close'} style={showGraphDetails ? { marginLeft: '800px' } : {}}>
-          <Grid item className={selectedNode.id ? 'node-drawer-open' : 'node-drawer-close'} style={selectedNode.id ? { marginRight: '500px' } : {}}>
-            <GraphContainer viewOnly={viewOnly} />
+            {/* Inline style has to be used here unfortunately */}
+            <Grid item className={showGraphDetails ? 'drawer-open' : 'drawer-close'} style={showGraphDetails ? { marginLeft: '800px' } : {}}>
+              <Grid item className={selectedNode.id ? 'node-drawer-open' : 'node-drawer-close'} style={selectedNode.id ? { marginRight: '500px' } : {}}>
+                <GraphContainer viewOnly={viewOnly} />
+              </Grid>
+            </Grid>
           </Grid>
-        </Grid>
-      </Grid>
 
-      <Prompt
-        when={!viewOnly}
-        message="Any unsaved changes will be lost!"
-      />
+          <Prompt
+            when={!viewOnly}
+            message="Any unsaved changes will be lost!"
+          />
+        </>
+      )}
     </div>
   );
 };
@@ -60,6 +78,7 @@ const mapStateToProps = (state) => ({
   elements: state.graph.present.elements,
   selectedNode: state.graph.present.selectedNode,
   showGraphDetails: state.graph.present.showGraphDetails,
+  username: state.user.username,
 });
 
 export default connect(mapStateToProps)(GraphPage);
